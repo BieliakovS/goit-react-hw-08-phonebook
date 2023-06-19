2c49113498623738bfd452cd1602a46734177229
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-06-19 23:53:01 +0300

    mentor corrects

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './reducers/store';
+import { store } from './reducers/store';
 import App from './components/App';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>,
   rootElement
 );
